731951a19c3eabe824c145ab8f83346e9983f93b
Henrietta Onoge <hetty8004@gmail.com>
2024-01-20 21:39:18 -0800

    updated task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,26 @@
-#!/usr/bin/pup
+# Using Puppet, install flask from pip3.
+
+# Requirements:
+
 # Installs flask
+# Version must be 2.1.0
+
+Package { 'python3.8':
+    ensure => '3.8.10',
+}
+
+Package { 'python3-pip':
+    ensure => installed,
+}
 
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+    ensure   => '2.1.0',
+    require  => Package['python3-pip'],
+    provider => 'pip',
+}
+
+package { 'Werkzeug':
+    ensure   => '2.2.2',
+    require  => Package['python3-pip'],
+    provider => 'pip',
 }
